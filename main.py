6073817387355c5b08c957f4a75d2002f1906b77
Priyanshu Parth <73892924+Priyanshuparth@users.noreply.github.com>
2023-05-14 18:57:44 +0530

    takeCommand

--- main.py
+++ main.py
@@ -8,8 +8,19 @@ def say(text):
     text_to_speech.runAndWait()
 
 
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        query = r.recognize_google(audio,language="en-in")
+        print(f"User Said: {query}")
+        return query
+
 
 if __name__ == '__main__':
     print('Pycham')
-    say("hello everyone to python")
-    
+    say("hello Abhijeet hehehe")
+    print("Listning...")
+    text= takeCommand()
+    say(text)
