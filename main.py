f7c1a387fcd1983a98db63e2e7856fc8ef2193d8
Priyanshu Parth <73892924+Priyanshuparth@users.noreply.github.com>
2023-05-18 12:57:33 +0530

    news

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import pyttsx3
 import webbrowser
 import os
 import openai
+from gnewsclient import gnewsclient
 from playsound import playsound
+import datetime
+client = gnewsclient.NewsClient(language='english',location='india',max_results=3)
 
 def say(text):
     text_to_speech = pyttsx3.init()
@@ -14,10 +17,10 @@ def say(text):
 def takeCommand():
     r=sr.Recognizer()
     with sr.Microphone() as source:
-        # r.pause_threshold = 1
+        r.pause_threshold = 0.8
         audio = r.listen(source)
         try:
-            print("Recognixing...")
+            print("Recognizing...")
             query = r.recognize_google(audio,language="en-in")
             print(f"User Said: {query}")
             say(query)
@@ -25,9 +28,22 @@ def takeCommand():
         except Exception as e:
             return "Some error Occured Sorry from Neom"
 
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        say("Good Morning")
+
+    elif hour>=12 and hour<18:
+        say("Good Afternoon")
+
+    else:
+        say("Good Evening")
+
+    say("Why to fear if neom is here")
 
 if __name__ == '__main__':
-    say("Hello I am NEOM A.I")
+    # say("Hello I am NEOM")
+    wish()
     while True:
         print("Listning...")
         query= takeCommand()
@@ -40,3 +56,15 @@ if __name__ == '__main__':
         if "open music" in query:
             musicPath = 'C:\\Users\\Priyanshu\\Downloads\\prop-plane-14513.mp3'
             os.system(f"open {musicPath}")
+
+        if "the time" in query:
+            strftime=datetime.datetime.now().strftime("%H:%M:%S") 
+            say(f"The time is {strftime}")
+
+        if "news" in query:
+            news_list = client.get_news()
+            for item in news_list:
+                st = item['title'].split(' - ', 1)[0]
+                print(st)
+                say(st)
+        
